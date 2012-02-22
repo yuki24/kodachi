51ef490bdd5ceb5674770e031b1c8acd2a760188
Yuki Nishijima <mail@yukinishijima.net>
2012-02-23 02:03:15 +0900

    use splat to get a path.

--- app.rb
+++ app.rb
@@ -5,17 +5,17 @@ helpers do
   end
 end
 
-get '/full/:path' do
-  load_file_from_aws(params[:path])
+get '/full/*' do |path|
+  load_file_from_aws(path)
   @image.content.value
 end
 
-get '/rfit/:width/:height/:path' do
-  load_file_from_aws(params[:path])
-  @image.resize_to_fit(params[:width].to_i,params[:height].to_i).to_blob
+get '/rfit/*/*/*' do |width, height, path|
+  load_file_from_aws(path)
+  @image.resize_to_fit(width.to_i, height.to_i).to_blob
 end
 
-get '/rfill/:width/:height/:path' do
-  load_file_from_aws(params[:path])
-  @image.resize_to_fill(params[:width].to_i,params[:height].to_i).to_blob
+get '/rfill/*/*/*' do |width, height, path|
+  load_file_from_aws(path)
+  @image.resize_to_fill(width.to_i, height.to_i).to_blob
 end
